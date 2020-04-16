fad490eac171f74cdc143d4ce3b3db6a4e9f814d
Eimi Okuno <eimi.okuno@bbc.co.uk>
2020-04-16 15:22:03 +0900

    Added URL so we can get actual media URLs to playback

--- functions/inventoryChecker/index.js
+++ functions/inventoryChecker/index.js
@@ -12,9 +12,7 @@ const deleteFirestore = async (admin, object) => {
   console.log(`[START] Deleting item ${id} for user ${uid} in ${folder}`);
 
   try {
-    await getUserCollection(admin, uid, folder)
-      .doc(id)
-      .delete();
+    await getUserCollection(admin, uid, folder).doc(id).delete();
     console.log(`[COMPLETE] Deleted item ${id} for user ${uid} in ${folder}`);
   } catch (e) {
     console.error(
@@ -39,7 +37,8 @@ const updateFirestore = async (admin, object) => {
     contentType: object.contentType,
     md5Hash: object.md5Hash,
     timeCreated: object.timeCreated,
-    duration: duration
+    duration: duration,
+    mediaUrl: object.mediaLink,
   };
 
   console.log(
@@ -48,9 +47,7 @@ const updateFirestore = async (admin, object) => {
     )}`
   );
   try {
-    await getUserCollection(admin, uid, folder)
-      .doc(id)
-      .set(update);
+    await getUserCollection(admin, uid, folder).doc(id).set(update);
     console.log(`[COMPLETE] Set item ${id} for user ${uid} in ${folder}`);
   } catch (e) {
     console.error(
