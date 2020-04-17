2a286488ec9a8b89bc56d1b084f836b7bf321e04
Eimi Okuno <eimi.okuno@bbc.co.uk>
2020-04-17 23:08:30 +0900

    Changing the media URL to a ref so that we can get download URL dynamically for streaming the video.

--- functions/inventoryChecker/index.js
+++ functions/inventoryChecker/index.js
@@ -11,80 +11,106 @@ const getUserCollection = (admin, uid, collection) => {
 };
 
 const deleteFirestore = async (admin, object) => {
-  const { id, userId, folder } = object.metadata;
-  console.log(`[START] Deleting item ${id} for user ${userId} in ${folder}`);
+  const { id: transcriptId, userId, folder } = object.metadata;
+  console.log(
+    `[START] Deleting item ${transcriptId} for user ${userId} in ${folder}`
+  );
 
   try {
-    await getUserCollection(admin, userId, folder).doc(id).delete();
+    await getUserCollection(admin, userId, folder).doc(transcriptId).delete();
     console.log(
-      `[COMPLETE] Deleted item ${id} for user ${userId} in ${folder}`
+      `[COMPLETE] Deleted item ${transcriptId} for user ${userId} in ${folder}`
     );
   } catch (e) {
     console.error(
-      `[ERROR] Failed to delete item ${id} for user ${userId} in ${folder}: `,
+      `[ERROR] Failed to delete item ${transcriptId} for user ${userId} in ${folder}: `,
       e
     );
   }
 };
 
-const updateFirestore = async (admin, object) => {
-  const {
-    metadata,
-    size,
-    contentType,
-    md5Hash,
-    timeCreated,
-    selfLink,
-  } = object;
 
-  const { id, userId, projectId, folder, originalName, duration } = metadata;
+const getUserUpdate = (object) => {
+  const { metadata, size, contentType, md5Hash, timeCreated, name } = object;
+  const { originalName, duration } = metadata;
 
-  const userUpdate = {
+  return {
     originalName: originalName ? originalName : "",
     size: size,
     contentType: contentType,
     md5Hash: md5Hash,
     timeCreated: timeCreated,
     duration: duration ? duration : 0,
-    selfLink: selfLink,
+    ref: name
   };
+};
 
-  const mediaType = contentType.split("/")[0];
+const getTranscriptionUpdate = (object) => {
+  const {id, contentType} = object;
+  return {
+    media: { type: contentType, ref: name },
+  }
+}
 
-  const transcriptionUpdate = { media: { type: mediaType, url: selfLink } };
-
+const updateUserWithFile = async (
+  admin,
+  userUpdate,
+  id,
+  userId,
+  folder
+) => {
   try {
     console.log(
       `[START] Setting item ${id} for user ${userId} in ${folder} with: ${JSON.stringify(
         userUpdate
       )}`
     );
-    await getUserCollection(admin, userId, folder).doc(id).set(userUpdate);
-    console.log(`[COMPLETE] Set item ${id} for user ${userId} in ${folder}`);
+    await getUserCollection(admin, userId, folder)
+      .doc(id)
+      .set(userUpdate);
+    console.log(
+      `[COMPLETE] Set item ${id} for user ${userId} in ${folder}`
+    );
   } catch (e) {
     console.error(
       `[ERROR] Failed to set item ${id} for user ${userId} in ${folder}: `,
       e
     );
   }
+  return userUpdate;
+};
 
+const updateTranscription = async (admin, update, transcriptId, projectId) => {
   try {
     console.log(
-      `[START] Updating transcription item ${id} in project ${projectId} with: ${JSON.stringify(
-        transcriptionUpdate
+      `[START] Updating transcription item ${transcriptId} in project ${projectId} with: ${JSON.stringify(
+        update
       )}`
     );
     await getTranscriptsCollection(admin, projectId)
-      .doc(id)
-      .update(transcriptionUpdate);
+      .doc(transcriptId)
+      .update(update);
+
     console.log(
-      `[COMPLETE] Updating transcription item ${id} in project ${projectId}`
+      `[COMPLETE] Updating transcription item ${transcriptId} in project ${projectId}`
     );
   } catch (e) {
     console.error(
-      `[ERROR] Failed transcription item ${id} in project ${projectId}:`,
+      `[ERROR] Failed transcription item ${transcriptId} in project ${projectId}:`,
       e
     );
+  }
+};
+
+const updateFirestore = async (admin, object) => {
+  const { id: transcriptId, userId, projectId, folder } = object.metadata;
+
+  const userUpdate = getUserUpdate(object);
+  await updateUserWithFile(admin, userUpdate, transcriptId, userId, folder);
+
+  if (folder === "uploads") {
+    const transcriptionUpdate = getTranscriptionUpdate(object)
+    await updateTranscription(admin, transcriptionUpdate, transcriptId, projectId);
   }
 };
 
