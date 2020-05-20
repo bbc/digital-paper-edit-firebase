366776a0ef4c325b17d3195127bbc42a8d8278f5
Eimi Okuno <eimi.okuno@bbc.co.uk>
2020-05-20 22:09:06 +0900

    Lazy loading

--- src/Components/PaperEditor/TranscriptsContainer/TranscriptTabContent/index.js
+++ src/Components/PaperEditor/TranscriptsContainer/TranscriptTabContent/index.js
@@ -1,8 +1,7 @@
 /* eslint-disable no-undef */
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState, Suspense } from 'react';
 import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
-import Paragraph from '../Paragraphs/Paragraph';
 import SearchBar from '../SearchBar'; // move to same folder + rename to SearchTool
 import Collection from '../../../Firebase/Collection';
 import TranscriptMenu from './TranscriptMenu';
@@ -10,6 +9,10 @@ import getTimeFromUserWordsSelection from '../get-user-selection.js';
 import paragraphWithAnnotations from '../Paragraphs/add-annotations-to-words-in-paragraphs.js';
 import groupWordsInParagraphsBySpeakers from '../Paragraphs/group-words-by-speakers.js';
 import removePunctuation from '../../../../Util/remove-punctuation';
+
+const Paragraph = React.lazy(() => import('../Paragraphs/Paragraph'));
+// import InfiniteLoader from 'react-window-infinite-loader';
+// import { FixedSizeList as List } from 'react-window';
 
 const TranscriptTabContent = (props) => {
   const { transcriptId, projectId, title, firebase, media, transcript } = props;
@@ -156,7 +159,7 @@ const TranscriptTabContent = (props) => {
   }, [ isHighlighting, searchString ]);
 
   useEffect(() => {
-    const getParagraphs = () => {
+    const getAnnotatedParagraphs = () => {
       const groupedParagraphs = groupWordsInParagraphsBySpeakers(
         transcript.words,
         transcript.paragraphs
@@ -171,7 +174,7 @@ const TranscriptTabContent = (props) => {
       transcript.words &&
       annotations
     ) {
-      setParagraphs(getParagraphs(transcript));
+      setParagraphs(getAnnotatedParagraphs(transcript));
     }
 
     return () => {};
@@ -566,7 +569,12 @@ const TranscriptTabContent = (props) => {
           style={ { height: cardBodyHeight, overflow: 'scroll' } }
         >
           {highlights}
-          {paragraphs ? Paragraphs : null}
+          <Suspense fallback={
+            <div>Loading...</div>
+          }>
+            {paragraphs ? Paragraphs : null}
+          </Suspense>
+
         </Card.Body>
       </Card>
     </>
