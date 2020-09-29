0745d689ab73a283ee6dbe6bc9edc7b71bc6be9a
Eimi Okuno <eimi.okuno@bbc.co.uk>
2020-09-29 20:31:06 +0100

    [refactor] temporary commit for changing the transcripts tab container # [<tag>] (If applied, this commit will...) <subject> (Max 72 char) # |<----   Preferably using up to 50 chars   --->|<------------------->| # Example: # [feat] Implement automated commit messages
    # (Optional) Explain why this change is being made
    # |<----   Try To Limit Each Line to a Maximum Of 72 Characters   ---->|
    
    # (Optional) Provide links or keys to any relevant tickets, articles or other resources
    # Example: Github issue #23
    
    # --- COMMIT END ---
    # Tag can be
    #    feat     (new feature)
    #    fix      (bug fix)
    #    refactor (refactoring code)
    #    style    (formatting, missing semi colons, etc; no code change)
    #    doc      (changes to documentation)
    #    test     (adding or refactoring tests; no production code change)
    #    version  (version bump/new release; no production code change)
    #    jsrXXX   (Patches related to the implementation of jsrXXX, where XXX the JSR number)
    #    jdkX     (Patches related to supporting jdkX as the host VM, where X the JDK version)
    #    dbg      (Changes in debugging code/frameworks; no production code change)
    #    license  (Edits regarding licensing; no production code change)
    #    hack     (Temporary fix to make things move forward; please avoid it)
    #    WIP      (Work In Progress; for intermediate commits to keep patches reasonably sized)
    #    defaults (changes default options)
    #
    # Note: Multiple tags can be combined, e.g. [fix][jsr292] Fix issue X with methodhandles
    # --------------------
    # Remember to:
    #   * Capitalize the subject line
    #   * Use the imperative mood in the subject line
    #   * Do not end the subject line with a period
    #   * Separate subject from body with a blank line
    #   * Use the body to explain what and why vs. how
    #   * Can use multiple lines with "-" or "*" for bullet points in body
    # --------------------

--- src/Components/PaperEditor/TranscriptsContainer/index.js
+++ src/Components/PaperEditor/TranscriptsContainer/index.js
@@ -1,16 +1,79 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Nav from 'react-bootstrap/Nav';
 import Tab from 'react-bootstrap/Tab';
+import TranscriptTabContent from './TranscriptTabContent';
+// import getTimeFromUserWordsSelection from './get-user-selection.js';
+import paragraphWithAnnotations from './Paragraphs/add-annotations-to-words-in-paragraphs.js';
 
 import PropTypes from 'prop-types';
 
 import { withAuthorization } from '../../Session';
 import TranscriptNavItem from './TranscriptNavItem';
-import TranscriptTabPane from './TranscriptTabPane';
 
-const TranscriptsContainer = ({ transcripts, projectId, firebase }) => {
+const TranscriptsContainer = ({ transcripts: trs, projectId, collections }) => {
+
+  const [ labels, setLabels ] = useState([]);
+  const [ transcripts, setTranscripts ] = useState(trs);
+
+  // const mediaType = media ? media.type : '';
+
+  const getSpeakerLabels = (paragraphs) => {
+    const speakerSet = paragraphs.reduce((uniqueSpeakers, p) => {
+      uniqueSpeakers.add(p.speaker);
+
+      return uniqueSpeakers;
+    }, new Set());
+
+    return Array.from(speakerSet).map((speaker) => ({
+      value: speaker,
+      label: speaker,
+    }));
+  };
+
+  useEffect(() => {
+
+    const getTranscript = async tr => {
+      const transcript = await collections.getTranscriptWithDecompression(projectId, tr.id);
+      const transcriptMetadata = {
+        media: { url: '' },
+        annotations: [],
+        annotatedParagraphs: [],
+        speakers: []
+      };
+
+      try {
+
+        transcriptMetadata.media.url = transcript.media ? await collections.getTranscriptMediaUrl(projectId, tr.id) : '';
+        transcriptMetadata.annotations = collections.getAnnotations(projectId, tr.id);
+        console.log(transcript, transcriptMetadata.annotations);
+        transcriptMetadata.annotatedParagraphs = paragraphWithAnnotations(transcript.grouped, transcript.annotations);
+        console.log(transcriptMetadata.annotatedParagraphs);
+        transcriptMetadata.speakers = transcript.annotatedParagraphs ? getSpeakerLabels(transcript.annotatedParagraphs) : [];
+        console.log(transcriptMetadata);
+
+        return { ...transcript, ...transcriptMetadata };
+      } catch (error) {
+        console.error(error, 'Could not parse transcripts');
+      }
+
+      return transcript;
+    };
+
+    const getTranscripts = async () => {
+      const transcriptsWithData = await Promise.all(trs.map(async tr => await getTranscript(tr)));
+      setTranscripts(transcriptsWithData);
+    };
+
+    if (collections) {
+      setLabels(collections.getProjectLabels(projectId));
+      getTranscripts();
+    }
+
+    return () => {};
+  }, [ labels, collections, projectId, trs ]);
+
   const transcriptsElNav = transcripts.map((transcript) => (
     <TranscriptNavItem
       key={ transcript.id }
@@ -19,17 +82,130 @@ const TranscriptsContainer = ({ transcripts, projectId, firebase }) => {
       status={ transcript.status }
     />
   ));
-  const transcriptsElTab = transcripts.map((transcript) => (
-    <TranscriptTabPane
-      key={ transcript.id }
-      transcriptId={ transcript.id }
-      groupedc={ transcript.groupedc }
-      media={ transcript.media }
-      title={ transcript.title }
-      projectId={ projectId }
-      firebase={ firebase }
-    />
-  ));
+
+  const createAnnotation = async (transcriptId, newAnnotation) => {
+    const item = await collections.createAnnotation(projectId, transcriptId, newAnnotation);
+    collections.updateAnnotation(projectId, transcriptId, { id: item.id });
+
+    return { ...newAnnotation, id: item.id } ;
+  };
+
+  const deleteAnnotation = (annotationId, transcriptId) => {
+    collections.deleteAnnotation(projectId, transcriptId, annotationId);
+  };
+
+  const handleCreateAnnotation = (transcriptId, newAnnotation) => {
+    createAnnotation(transcriptId, newAnnotation);
+  };
+
+  const handleDeleteAnnotation = (annotationId, transcriptId) => {
+    deleteAnnotation(annotationId, transcriptId);
+  };
+
+  const updateAnnotation = (annotationId, transcriptId, update) => {
+    collections.updateAnnotation(projectId, transcriptId, update);
+  };
+
+  const handleEditAnnotation = (annotationId, transcriptId, newAnnotationToEdit) => {
+    updateAnnotation(annotationId, transcriptId, newAnnotationToEdit);
+  };
+
+  const createLabel = async (newLabel) => {
+    const label = await collections.createLabel(projectId, newLabel);
+    setLabels(() => [ ...labels, label ]);
+  };
+
+  const handleCreateLabel = (newLabel) => {
+    createLabel(newLabel);
+  };
+
+  const handleEditLabel = (labelId, updatedLabel) => {
+    collections.updateLabel(labelId, updatedLabel);
+    setLabels(() => [ ...labels, updatedLabel ]);
+  };
+
+  const handleDeleteLabel = (labelId) => {
+    const tempLabels = labels;
+    tempLabels.splice(labelId, 1);
+    setLabels(tempLabels);
+    collections.deleteLabels(labelId);
+  };
+
+  const handleLabelSelection = (e, labelId) => {
+    const tempLabels = JSON.parse(JSON.stringify(labels));
+
+    const previousActiveLabel = tempLabels.find((label) => label.active);
+    if (previousActiveLabel) {
+      previousActiveLabel.active = false;
+    }
+    const activeLabel = tempLabels.find((label) => label.id === labelId);
+    activeLabel.active = true;
+
+    setLabels(tempLabels);
+  };
+
+  const getTranscriptTabs = () => {
+    return transcripts.map((transcript) => {
+      const { id, annotations, media, title, speakers, annotatedParagraphs } = transcript;
+
+      return (
+        <Tab.Pane eventKey={ id }
+          key={ id }
+        >
+          <TranscriptTabContent
+            projectId={ projectId }
+            transcriptId={ id }
+            media={ media }
+            title={ title }
+
+            annotations={ annotations }
+            speakers={ speakers }
+            annotatedParagraphs={ annotatedParagraphs }
+
+            handleCreateAnnotation={ handleCreateAnnotation }
+            handleDeleteAnnotation={ handleDeleteAnnotation }
+            handleEditAnnotation={ handleEditAnnotation }
+
+            handleCreateLabel={ handleCreateLabel }
+            handleDeleteLabel={ handleDeleteLabel }
+            handleEditLabel={ handleEditLabel }
+            handleLabelSelection={ handleLabelSelection }
+          />
+        </Tab.Pane>
+      );
+    });
+  };
+
+  // const transcriptsElTab = transcripts.map((transcript) => {
+  //   const { id, annotations, media, title, speakers, annotatedParagraphs } = transcript;
+
+  //   return (
+  //     <Tab.Pane eventKey={ id }
+  //       key={ id }
+  //     >
+  //       <TranscriptTabContent
+  //         projectId={ projectId }
+  //         transcriptId={ id }
+  //         media={ media }
+  //         title={ title }
+
+  //         annotations={ annotations }
+  //         speakers={ speakers }
+  //         annotatedParagraphs={ annotatedParagraphs }
+
+  //         handleCreateAnnotation={ handleCreateAnnotation }
+  //         handleDeleteAnnotation={ handleDeleteAnnotation }
+  //         handleEditAnnotation={ handleEditAnnotation }
+
+  //         handleCreateLabel={ handleCreateLabel }
+  //         handleDeleteLabel={ handleDeleteLabel }
+  //         handleEditLabel={ handleEditLabel }
+  //         handleLabelSelection={ handleLabelSelection }
+  //       />
+  //     </Tab.Pane>
+  //   );
+  // });
+
   const getDefaultActiveKey = () => {
     const doneItem = transcripts.find(transcript => transcript.status === 'done');
     if (doneItem) {
@@ -60,7 +236,7 @@ const TranscriptsContainer = ({ transcripts, projectId, firebase }) => {
           </Col>
           <Col sm={ 9 }>
             <Tab.Content>
-              {transcriptsElTab}
+              {transcripts ? getTranscriptTabs() : null}
             </Tab.Content>
           </Col>
         </Row>
@@ -70,9 +246,23 @@ const TranscriptsContainer = ({ transcripts, projectId, firebase }) => {
 };
 
 TranscriptsContainer.propTypes = {
+  collections: PropTypes.shape({
+    createAnnotation: PropTypes.func,
+    createLabel: PropTypes.func,
+    deleteAnnotation: PropTypes.func,
+    deleteLabels: PropTypes.func,
+    getAnnotations: PropTypes.func,
+    getProjectLabels: PropTypes.func,
+    getTranscriptMediaUrl: PropTypes.func,
+    getTranscriptWithDecompression: PropTypes.func,
+    updateAnnotation: PropTypes.func,
+    updateLabel: PropTypes.func
+  }),
+  media: PropTypes.shape({
+    type: PropTypes.any
+  }),
   projectId: PropTypes.any,
   transcripts: PropTypes.any,
-  firebase: PropTypes.any,
 };
 
 const condition = (authUser) => !!authUser;
