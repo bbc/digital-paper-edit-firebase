17031e9e5d65054e82a2c7808cc8c6ecf2749fde
Eimi Okuno <eimi.okuno@bbc.co.uk>
2021-04-09 16:44:31 +0100

    [doc]inline comments for 2 routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,17 +1,16 @@
-// export const SIGN_IN = '/'; // same as sign in
 export const LANDING = '/';
-// export const SIGN_UP = '/signup';
 export const SIGN_IN = '/signin';
-// export const HOME = '/home';
-// export const ACCOUNT = '/account';
-// export const PASSWORD_FORGET = '/pw-forget';
 export const ADMIN = '/admin';
-// export const ADMIN_DETAILS = '/admin/:id';
 export const PROJECTS = '/projects';
 export const WORKSPACE = '/projects/:projectId';
 export const PAPER_EDITOR = '/projects/:projectId/paperedits/:papereditId';
+
+// Same routes point to transcript editor for regression
+// https://github.com/bbc/digital-paper-edit-firebase/pull/173
 export const TRANSCRIPT_EDITOR =
   '/projects/:projectId/transcripts/:transcriptId';
 export const TRANSCRIPT_EDITOR_CORRECT =
   '/projects/:projectId/transcripts/:transcriptId/correct';
+// ============================================================
+
 export const USERS = '/users';
