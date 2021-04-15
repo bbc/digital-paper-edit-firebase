9a8048b5db8b7b87305e55658eb1803230e691a7
Tamsin Green <tamsin.green@bbc.co.uk>
2021-04-15 15:24:42 +0100

    Use python 2 modules

--- public/server.py
+++ public/server.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
-import http.server
-import socketserver
-class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+import SimpleHTTPServer, SocketServer
+class MyHttpRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/status':
             self.send_response(200)
         else: 
             self.path = '/index.html'
-            return http.server.SimpleHTTPRequestHandler.do_GET(self)
+            return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 # Create an object of the above class
 handler_object = MyHttpRequestHandler
 PORT = 8080
-my_server = socketserver.TCPServer(("", PORT), handler_object)
+my_server = SocketServer.TCPServer(("", PORT), handler_object)
 # Start the server
 print("serving at port", PORT)
 my_server.serve_forever()
