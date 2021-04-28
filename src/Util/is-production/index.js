6ff3028400496b6673bf8d4693720abfa634d4ec
Tamsin Green <tamsin.green@bbc.co.uk>
2021-04-28 18:03:37 +0100

    Chnages from code review

--- src/Util/is-production/index.js
+++ src/Util/is-production/index.js
@@ -3,12 +3,7 @@ import url from 'url';
 const isProduction = () => {
   const parsedUrl = url.parse(window.location.href, true);
 
-  if ((parsedUrl.host) === 'digital-paper-edit.tools.bbc.co.uk') {
-    return true;
-  }
-
-  return false;
-
+  return parsedUrl.host === 'digital-paper-edit.tools.bbc.co.uk';
 };
 
 export default isProduction;
