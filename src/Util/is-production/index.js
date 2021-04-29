355d7e4146e184104226d39199a8584ed4b92dee
aniablaziak <anblaziak@gmail.com>
2021-04-29 15:10:40 +0100

    feat: #224 check for prod firebase url

--- src/Util/is-production/index.js
+++ src/Util/is-production/index.js
@@ -3,7 +3,7 @@ import url from 'url';
 const isProduction = () => {
   const parsedUrl = url.parse(window.location.href, true);
 
-  return parsedUrl.host === 'digital-paper-edit.tools.bbc.co.uk';
+  return parsedUrl.host === 'digital-paper-edit.tools.bbc.co.uk' || parsedUrl.host === 'digital-paper-edit-prod.firebaseapp.com';
 };
 
 export default isProduction;
