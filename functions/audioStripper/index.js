9781ccaaf940bb02f5526cb6dd18da2244c1a0c8
tamsingreen <tamsingreen@users.noreply.github.com>
2021-05-19 13:53:26 +0100

    258: Add more logs

--- functions/audioStripper/index.js
+++ functions/audioStripper/index.js
@@ -1,36 +1,39 @@
-const ffmpeg = require("fluent-ffmpeg");
-const { info, error } = require("firebase-functions/lib/logger");
-const { getStorageSignedUrl } = require("../utils/firebase");
+const ffmpeg = require('fluent-ffmpeg');
+const { info, error } = require('firebase-functions/lib/logger');
+const { getStorageSignedUrl } = require('../utils/firebase');
 
 // Will NOT work for MP4 Streams
 // https://stackoverflow.com/questions/23002316/ffmpeg-pipe0-could-not-find-codec-parameters/40028894#40028894
-const convertStreamToAudio = (inputStream, outputStream) => {
+const convertStreamToAudio = (inputStream, outputStream, jobData) => {
   return new Promise((resolve, reject) => {
     ffmpeg(inputStream)
       .noVideo()
-      .audioCodec("pcm_s16le")
+      .audioCodec('pcm_s16le')
       .audioChannels(1)
-      .toFormat("wav")
+      .toFormat('wav')
       .audioFrequency(16000)
-      .on("start", (cmd) => {
+      .on('start', (cmd) => {
         // console.debug("Started " + cmd);
+        info(`[START] ffmpeg job ${ jobData } command ${ cmd }`);
       })
-      .on("codecData", (data) => {
+      .on('codecData', (data) => {
         // console.debug(`Input is ${data.audio} audio with ${data.video} video`);
       })
-      .on("error", (err, stdout, stderr) => {
+      .on('error', (err, stdout, stderr) => {
         // console.debug(err.message); //this will likely return "code=1" not really useful
         // console.debug("stdout:\n" + stdout);
         // console.debug("stderr:\n" + stderr); //this will contain more detailed debugging info
+        error('[ERROR] ffmpeg job', jobData, ', error:', err, ', stderr:', stderr);
         reject(err);
       })
-      .on("progress", (progress) => {
+      .on('progress', (progress) => {
         // console.debug(progress);
         // console.debug(`Processing: ${progress.percent}% done`);
       })
-      .on("end", (stdout, stderr) => {
+      .on('end', (stdout, stderr) => {
         // console.debug(stdout, stderr);
         // console.debug("Transcoding succeeded!");
+        info('[COMPLETE] ffmpeg job', jobData);
         resolve();
       })
       .pipe(outputStream, { end: true });
@@ -41,19 +44,19 @@ exports.createHandler = async (snap, bucket, context) => {
   const { userId, itemId } = context.params;
   const { projectId, originalName, duration } = snap.data();
 
-  const srcFile = bucket.file(`users/${userId}/uploads/${itemId}`);
-  const outFile = bucket.file(`users/${userId}/audio/${itemId}`);
+  const srcFile = bucket.file(`users/${ userId }/uploads/${ itemId }`);
+  const outFile = bucket.file(`users/${ userId }/audio/${ itemId }`);
 
   const metadata = {
     metadata: {
       id: itemId,
       userId: userId,
       projectId: projectId,
-      folder: "audio",
+      folder: 'audio',
       originalName: originalName,
       duration: duration,
     },
-    contentType: "audio/wav",
+    contentType: 'audio/wav',
   };
 
   const writeStream = outFile.createWriteStream({
@@ -64,22 +67,24 @@ exports.createHandler = async (snap, bucket, context) => {
     item: itemId,
     project: projectId,
     user: userId
-  }
+  };
 
   try {
+    info('[START] audioStripper. Fetching signed storage URL for', jobData);
     const sourceUrl = await getStorageSignedUrl(srcFile);
-    info(`[START] Streaming, transforming file ${sourceUrl} to audio`, jobData);
-    await convertStreamToAudio(sourceUrl[0], writeStream);
+    info(`[IN PROGRESS] Convert file ${ sourceUrl } to wav audio`, jobData);
+    await convertStreamToAudio(sourceUrl[0], writeStream, jobData);
   } catch (err) {
     return error(
-      "[ERROR] Could not stream / transform audio file: ",
+      '[ERROR] Could not stream / transform audio file: ',
       {
         ...jobData,
         err
       }
     );
   }
+
   return info(
-    `[COMPLETE] Uploaded audio file`, jobData
+    '[COMPLETE] Uploaded audio file', jobData
   );
 };
